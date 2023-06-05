7525fd3f84ca0dd8923e1f7670d9467a317c154a
Doug Leasure <doug.leasure@protonmail.com>
2023-06-05 18:22:27 +0100

    oecd api urls to columns for readability

--- src/scrapers/oecd/oecd_data.R
+++ src/scrapers/oecd/oecd_data.R
@@ -122,7 +122,7 @@ consistent_columns = function(
 
 # Load API URLs and define ID variables for data sets
 id.vars = c("Country", "iso3", "Region", "Gender", "Year")
-url_list = as.list(as.character(read.delim(file.path(srcdir, 'oecd_api_urls.txt'), sep = ',', header = F, colClasses = "character")[1, ]))
+url_list = as.list(read.delim(file.path(srcdir, 'oecd_api_urls.txt'), sep = ',', header = F, colClasses = "character")[,1])
 
 # Regional statistics on education (selection of countries and regions)
 oecd_education_country = api_to_data_set(url_list[[1]], id.vars)[[1]]
