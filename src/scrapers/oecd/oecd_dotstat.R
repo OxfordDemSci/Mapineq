21fc4038ee8cd6ae286c2dec36f53bf81070f80e
Doug Leasure <doug.leasure@protonmail.com>
2023-06-05 18:22:54 +0100

    successful test of dotstat.oecd query example

--- src/scrapers/oecd/oecd_dotstat.R
+++ src/scrapers/oecd/oecd_dotstat.R
@@ -6,7 +6,7 @@ library(httr)
 library(jsonlite)
 library(tidyr)
 library(stringr)
-library(readsdmx)
+library(rsdmx)
 
 # working directory
 setwd(file.path(dirname(rstudioapi::getSourceEditorContext()$path), "..", "..", ".."))
@@ -16,5 +16,17 @@ srcdir <- file.path('src', 'scrapers', 'oecd')
 outdir <- file.path('out', 'oecd')
 dir.create(outdir, showWarnings=F, recursive=T)
 
-# country list
-countries <- c()
+# load data
+oecd_regions <- read.csv(file.path('in', 'oecd', 'regions', 'OECD_TL3_2020.csv'))
+
+# mapineq countries
+oecd_regions <- oecd_regions[oecd_regions$mapineq, ]
+
+# query demographic variables
+query_string <- paste0('http://stats.oecd.org/restsdmx/sdmx.ashx/GetData/REGION_DEMOGR/2.',
+                       '.',
+                       'T.T.ALL.',
+                       '2021',
+                       '/all?')
+
+tl2_popwgt <- as.data.frame(readSDMX(query_string))
