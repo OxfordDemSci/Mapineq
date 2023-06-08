482f3f9b0642919b7d1eaa151d7390609973584d
Doug Leasure <doug.leasure@protonmail.com>
2023-06-08 18:07:12 +0100

    bulk scrape OECD regional databases (5/10 successful)

--- src/scrapers/oecd/oecd_restsdmx.R
+++ src/scrapers/oecd/oecd_restsdmx.R
@@ -1,10 +1,11 @@
 # cleanup
-rm(list = ls()); gc(); cat("\014"); try(dev.off(), silent = T); options(scipen = 999)
+rm(list=ls()); gc(); cat("\014"); try(dev.off(), silent=T); options(scipen=999)
 
 # working directory
-setwd(file.path(dirname(rstudioapi::getSourceEditorContext()$path), "..", "..", ".."))
+setwd(file.path(dirname(rstudioapi::getSourceEditorContext()$path), 
+                "..", "..", ".."))
 
-# directories
+# define directories
 srcdir <- file.path('src', 'scrapers', 'oecd')
 outdir <- file.path('out', 'oecd')
 dir.create(outdir, showWarnings=F, recursive=T)
@@ -12,17 +13,82 @@ dir.create(outdir, showWarnings=F, recursive=T)
 # load functions
 source(file.path(srcdir, 'functions.R'))
 
-# load data
-oecd_regions <- read.csv(file.path('in', 'oecd', 'regions', 'OECD_TL3_2020.csv'))
+# define queries
+query <- list(region_st = paste0('REGION_ST/',  # endpoint FAIL 400
+                                 '1+2+3.',  # TL
+                                 '.',  # REG_ID
+                                 '.',  # IND
+                                 ''),  # TIME
+              region_econom = paste0('REGION_ECONOM/',  # endpoint SUCCESS
+                                     '1+2+3.',  # TL
+                                     '.',  # REG_ID
+                                     'SNA_2008.',  # SERIES
+                                     '.',  # VAR
+                                     '.',  # MEAS,
+                                     'ALL.',  # POS
+                                     ''),  # TIME
+              region_busi_demog = paste0('REGION_BUSI_DEMOG/',  # endpoint FAIL 500
+                                     '1+2+3.',  # TL
+                                     '.',  # REG_ID
+                                     '.',  # VAR
+                                     '.',  # SECTOR
+                                     '.',  # SIZECLASS
+                                     ''),  # TIME
+              region_demogr = paste0('REGION_DEMOGR/',  # endpoint SUCCESS
+                                     '1+2+3.',  # TL
+                                     '.',  # REG_ID
+                                     '.',  # VAR
+                                     'T+F+M.',  # SEX
+                                     'ALL.',  # POS
+                                     '',  # TIME
+                                     '/all?'),
+              region_labour = paste0('REGION_LABOUR/',  # endpoint SUCCESS
+                                     '1+2+3.',  # TL
+                                     '.',  # REG_ID
+                                     '.',  # VAR
+                                     'T+F+M.',  # SEX
+                                     'ALL.',  # POS
+                                     ''),  # TIME
+              region_educat = paste0('REGION_EDUCAT/',  # endpoint FAIL 400
+                                     '.',  # LOCATION
+                                     '.',  # REG_ID
+                                     '.',  # IND
+                                     '.',  # ISC11
+                                     'T+F+M.',  # GENDER
+                                     '.',  # MEAS
+                                     ''),  # TIME
+              region_innovation = paste0('REGION_INNOVATION/',  # endpoint SUCCESS
+                                         '1+2+3.',  # TL
+                                         '.',  # REG_ID
+                                         '.',  # VAR
+                                         'ALL.',  # POS
+                                         ''),  # TIME
+              region_social = paste0('REGION_SOCIAL/',  # endpoint SUCCESS
+                                     '1+2+3.',  # TL
+                                     '.',  # REG_ID
+                                     '.',  # VAR
+                                     'ALL.',  # POS
+                                     ''),  # TIME
+              region_migrants = paste0('REGION_MIGRANTS/',  # endpoint FAIL 400
+                                       '1+2+3.',  # TL
+                                       '.',  # REG_ID
+                                       '.',  # ORIGIN
+                                       '.',  # IND
+                                       'ALL.',  # POS
+                                       ''),  # TIME
+              region_rwb = paste0('REGION_RWB/',  # endpoint FAIL 500
+                                 '1+2+3.',  # TL
+                                 '.',  # REG_ID
+                                 '.',  # VAR
+                                 '.',  # MEAS
+                                 'ALL.',  # POS
+                                 '')  # TIME
+)
 
-# mapineq countries
-oecd_regions <- oecd_regions[oecd_regions$mapineq, ]
-
-# GET REGION_DEMOGR
-dat_list <- get_region_demogr(oecd_key = oecd_regions)
-
-for(i in c('tl1', 'tl2', 'tl3')){
-  write.csv(dat_list[[i]], 
-            file = file.path(outdir, paste0('region_demogr_',i,'.csv')), 
-            row.names = F)
+# submit http requests
+dat <- list()
+for(endpoint in names(query)){
+  dat[[endpoint]] <- request_oecd(query = query[[endpoint]], 
+                                  fname = file.path(outdir, paste0(endpoint, '.csv')),
+                                  overwrite = FALSE)
 }
