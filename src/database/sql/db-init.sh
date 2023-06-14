5f1561c4c34955717c147c65bf8fb4802bb27c9e
Doug Leasure <doug.leasure@protonmail.com>
2023-06-14 16:04:42 +0100

    rename "docker" folder to "database"

---
+++ src/database/sql/db-init.sh
@@ -0,0 +1,31 @@
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c \
+"
+CREATE ROLE reader LOGIN PASSWORD '${POSTGRES_RPASS}';
+
+CREATE TABLE NUTS(
+  id VARCHAR(5) PRIMARY KEY,
+  level SMALLINT,
+  name VARCHAR(70),
+  urban SMALLINT,
+  mount SMALLINT,
+  coast SMALLINT,
+  nuts0_id CHAR(2),
+  nuts1_id VARCHAR(5),
+  nuts2_id VARCHAR(5),
+  nuts3_id VARCHAR(5),
+  tl_id VARCHAR(5),
+  geom GEOMETRY
+);
+GRANT SELECT ON NUTS TO reader;
+
+CREATE TABLE catalogue(
+  id INT PRIMARY KEY,
+  provider VARCHAR(5),
+  resource VARCHAR(40),
+  descr VARCHAR(300),
+  version VARCHAR(20),
+  url VARCHAR(150),
+  UNIQUE(provider, resource, version)
+);
+GRANT SELECT ON catalogue TO reader;
+"
