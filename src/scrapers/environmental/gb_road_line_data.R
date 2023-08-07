b0b1a6b6067d0fda42166ff0b29f4f21c51d1ccf
fverkroost <florianneverkroost@planet.nl>
2023-08-07 14:50:26 +0200

    Add British road data as line data example

---
+++ src/scrapers/environmental/gb_road_line_data.R
@@ -0,0 +1,21 @@
+# cleanup
+rm(list=ls()); gc(); cat("\014"); try(dev.off(), silent=T); options(scipen=999)
+
+# working directory
+setwd(file.path(dirname(rstudioapi::getSourceEditorContext()$path), 
+                "..", "..", ".."))
+
+# define directories
+srcdir <- file.path('src', 'scrapers', 'environmental')
+outdir <- file.path('out', 'environmental')
+dir.create(outdir, showWarnings=F, recursive=T)
+
+# load functions
+source(file.path(srcdir, 'functions.R'))
+
+# get data 
+url = "https://api.os.uk/downloads/v1/products/OpenRoads/downloads?area=GB&format=GeoPackage&redirect"
+temp = tempfile()
+download.file(url, temp)
+get_data('oproad_gb.shp', unzip(temp), outdir)
+unlink(temp)
