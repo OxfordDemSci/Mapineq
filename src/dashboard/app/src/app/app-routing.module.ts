01f9d07859c89ba37e2479e33ef197b3a6b7ce75
niekvanruler <n.j.van.ruler@rug.nl>
2024-07-05 15:05:43 +0200

    first version of datacatalogue

--- src/dashboard/app/src/app/app-routing.module.ts
+++ src/dashboard/app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {DatacatalogueComponent} from "./datacatalogue/datacatalogue.component";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'case/:id', component: DashboardComponent },
   { path: 'case/:id/:variant', component: DashboardComponent },
+  { path: 'datacatalogue', component: DatacatalogueComponent },
   { path: '**', component: DashboardComponent },
 
 ];
