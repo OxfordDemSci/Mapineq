6ba7e176389644db6434a8bb11d8aeffb3c463e9
niekvanruler <n.j.van.ruler@rug.nl>
2025-01-09 13:50:34 +0100

    added dark background map to the code

--- src/dashboard/app/src/app/result-map/result-map.component.ts
+++ src/dashboard/app/src/app/result-map/result-map.component.ts
@@ -171,6 +171,12 @@ export class ResultMapComponent implements OnInit, AfterViewInit, OnChanges {
         //     });
         // this.map.addLayer(this.layerMapOSM);
 
+        let CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 20
+        });
+
         let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
             maxZoom: 16
