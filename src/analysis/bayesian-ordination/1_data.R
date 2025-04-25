fc96d2ed4657e5308939e6bc45c72ebea1a54b2b
Doug Leasure <douglas.leasure@demography.ox.ac.uk>
2025-04-25 17:03:37 +0100

    data collection with selected variables

--- src/analysis/bayesian-ordination/1_data.R
+++ src/analysis/bayesian-ordination/1_data.R
@@ -6,12 +6,41 @@ rm(list = ls())
 gc()
 
 #---- USER OPTIONS ----#
+TEST <- FALSE
+n_test <- 100
+
 year <- 2020
 level <- 2
+
 drop_resources <- c("HLTH_CD_ACDR2")
+
+keep_resources <- c(
+  "pm25", # air particulates
+  "ookla", # internet speed
+  "TGS00050", # internet usage
+  "RD_E_GERDREG", # gross domestic expenditure on R&D
+  "TGS00064", # hospital beds
+  "HLTH_RS_BDSNS", # care beds
+  "TGS00058", # cancer deaths
+  "TGS00059", # heart disease deaths
+  "DEMO_R_MINFIND", # infant mortality
+  "HLTH_CD_YPERRTO", # peri- neo-natal mortality
+  "DEMO_R_MLIFEXP", # life expectancy
+  "EDUC_UOE_ENRA17", # pupils pre-primary
+  "EDUC_UOE_ENRA13", # distribution of students among education types
+  "TGS00109", # tertiary educational attainment
+  "EDAT_LFS_9918", # educational attainment
+  "TEPSR_LM220", # gender employment gap
+  "YTH_EMPL_110", # youth unemployment
+  "TGS00010", # employment rate
+  "EDAT_LFSE_33", # youth NEET
+  "TESPM050_R", # poverty reduction
+  "TGS00099", # population change (natural, migration, total)
+  "DEMO_R_FIND2" # fertility indicators
+)
 #----------------------#
 
-# source functions
+# load functions
 source(file.path(getwd(), "src", "analysis", "bayesian-ordination", "1_data_fun.R"))
 
 # directories
@@ -19,25 +48,51 @@ outdir <- file.path(getwd(), "wd", "out", "bayesian-ordination", "data")
 dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
 
 # get mapineq data catalogue
-catalogue <- get_catalogue(level)
+data_catalogue <- get_catalogue(level)
 
 # drop resources
-catalogue <- catalogue |> 
-  filter(!f_resource %in% drop_resources)
+if (length(drop_resources) > 0) {
+  data_catalogue <- data_catalogue |>
+    filter(!f_resource %in% drop_resources)
+}
 
-# keep data for selected year
-catalogue <- catalogue_for_year(catalogue, year)
+# keep resources
+if (length(keep_resources) > 0) {
+  data_catalogue <- data_catalogue |>
+    filter(f_resource %in% keep_resources)
+}
+
+# keep data only for selected year
+data_catalogue <- catalogue_for_year(data_catalogue, year)
 
 # add filters to catalogue
-catalogue <- catalogue_filters(catalogue, year, level)
+data_catalogue <- catalogue_filters(data_catalogue, year, level)
+
+# filter labels
+filter_dictionary <- filter_labels(data_catalogue)
 
 # expand catalogue to include all combinations of filters
-catalogue_expanded <- expand_catalogue(catalogue)
+catalogue_expanded <- expand_catalogue(data_catalogue)
+
+# reduce for testing
+if (TEST) {
+  i_select <- sample(x = 1:nrow(catalogue_expanded), size = min(nrow(catalogue_expanded), n_test))
+} else {
+  i_select <- 1:nrow(catalogue_expanded)
+}
 
 # get data for all items in the catalogue
-dat <- catalogue_data(catalogue_expanded, year, level)
+results <- catalogue_data(catalogue_expanded[i_select, ], year, level)
+
+dat <- bind_rows(results) 
+# TODO: Troubleshoot bind_rows() with columns of different sizes
+# ! Tibble columns must have compatible sizes.
+# • Size 332: Columns `TGS00058`, `TGS00059`, `EDUC_UOE_ENRA13`, `HLTH_CD_YPERRTO`, `ookla`, and 6 more.
+# • Size 334: Columns `TGS00064`, `EDAT_LFSE_33`, `DEMO_R_FIND2`, `TEPSR_LM220`, `DEMO_R_MINFIND`, and 5 more.
+# ℹ Only values of size one are recycled.
 
 # save to disk
 write.csv(dat, file.path(outdir, "data.csv"), row.names = FALSE)
-write.csv(catalogue, file.path(outdir, "catalogue.csv"), row.names = FALSE)
+write.csv(data_catalogue, file.path(outdir, "catalogue.csv"), row.names = FALSE)
 write.csv(catalogue_expanded, file.path(outdir, "catalogue_expanded.csv"), row.names = FALSE)
+write.csv(filter_dictionary, file.path(outdir, "filter_dictionary.csv"), row.names = FALSE)
--- src/analysis/bayesian-ordination/1_data_fun.R
+++ src/analysis/bayesian-ordination/1_data_fun.R
@@ -9,6 +9,10 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 
+
+#---- GET operations to retrieve data ----#
+
+# get the Mapineq data catalogue for a given NUTS level
 get_catalogue <- function(level) {
   response <- httr::GET(
     url = "https://api.mapineq.org",
@@ -16,48 +20,146 @@ get_catalogue <- function(level) {
     query = list(
       `_level` = level,
       limit = 10e3
-    ),
-    config = config(ssl_verifypeer = FALSE) # TODO: REMOVE WHEN SSL RENEWED
+    ) # , config = config(ssl_verifypeer = FALSE)  # ignores expired ssl certificate
   )
 
   if (!status_code(response) == 200) {
     print(paste("Error:", status_code(response)))
   } else {
     df_content <- jsonlite::fromJSON(content(response, "text", encoding = "UTF-8"))
+    df_content <- df_content |>
+      mutate(geo_level = level)
     return(df_content)
   }
 }
 
-catalogue_for_year <- function(catalogue, year) {
-  keep_resource <- c()
-  for (resource in catalogue$f_resource) {
-    response <- httr::GET(
-      url = "https://api.mapineq.org",
-      path = "functions/postgisftw.get_year_nuts_level_from_source/items.json",
-      query = list(
-        `_resource` = resource
-      ),
-      config = config(ssl_verifypeer = FALSE) # TODO: REMOVE WHEN SSL RENEWED
-    )
+# get years available for a given resource
+get_years <- function(resource) {
+  response <- httr::GET(
+    url = "https://api.mapineq.org",
+    path = "functions/postgisftw.get_year_nuts_level_from_source/items.json",
+    query = list(
+      `_resource` = resource,
+      limit = 10e3
+    ) # , config = config(ssl_verifypeer = FALSE) # TODO: REMOVE WHEN SSL RENEWED
+  )
 
-    if (!status_code(response) == 200) {
-      print(paste("Error:", status_code(response)))
-    } else {
-      df_content <- fromJSON(content(response, "text", encoding = "UTF-8"))
-      if (year %in% df_content$f_year) {
-        keep_resource <- c(keep_resource, resource)
-      }
-    }
+  if (!status_code(response) == 200) {
+    print(paste("Error:", status_code(response)))
+  } else {
+    df_content <- fromJSON(content(response, "text", encoding = "UTF-8"))
+    df_content <- df_content |>
+      mutate(data_year = year)
+    return(df_content)
   }
-  result <- catalogue |>
-    filter(f_resource %in% keep_resource)
+}
+
+# get all filters available for a given resource
+get_filters <- function(resource, year, level) {
+  response <- httr::GET(
+    url = "https://api.mapineq.org",
+    path = "functions/postgisftw.get_column_values_source_json/items.json",
+    query = list(
+      `_resource` = resource,
+      source_selections = toJSON(list(
+        year = year,
+        level = level,
+        selected = list(),
+        limit = 10e3
+      ), auto_unbox = TRUE)
+    ) # , config = config(ssl_verifypeer = FALSE)  # ignores expired ssl certificate
+  )
+  result <- content(response, "text", encoding = "UTF-8")
   return(result)
 }
 
+# get data for all administrative units for a given resource, year, nuts level, and resource filter values
+get_data <- function(resource, year, level, x_specs) {
+  X_JSON <- jsonlite::toJSON(x_specs, auto_unbox = TRUE)
+
+  response <- httr::GET(
+    url = "https://api.mapineq.org",
+    path = "functions/postgisftw.get_x_data/items.json",
+    query = list(
+      `_level` = level,
+      `_year` = year,
+      X_JSON = X_JSON,
+      limit = 10e3
+    ) # , config = config(ssl_verifypeer = FALSE)  # ignores expired ssl certificate
+  )
+
+  if (!status_code(response) == 200) {
+    message(paste0("[", resource, "] http status:", status_code(response)))
+  } else {
+    tryCatch(
+      {
+        df_content <- jsonlite::fromJSON(content(response, "text", encoding = "UTF-8"))
+        if (is.data.frame(df_content)) {
+          df_content <- df_content %>%
+            as_tibble() %>%
+            mutate(
+              indicator = resource,
+              geo_level = level,
+              data_year = year
+            ) %>%
+            rename(value = x) %>%
+            mutate(filters_json = as.character(X_JSON)) %>%
+            mutate(
+              filters = map(
+                filters_json,
+                ~ fromJSON(.x, simplifyVector = FALSE)
+              )
+            ) %>%
+            mutate(
+              combos = map(filters, ~ {
+                conds <- .x$conditions
+                named_values <- set_names(
+                  map_chr(conds, "value"),
+                  map_chr(conds, "field")
+                )
+                cross_df(named_values)
+              })
+            ) %>%
+            select(-filters_json, -filters) %>%
+            unnest(combos)
+
+          return(df_content)
+        } else {
+          message(paste0("No data returned for: \n", X_JSON))
+        }
+      },
+      warning = function(e) {
+        warning(paste0("WARNING: [", resource, "] ", e))
+      },
+      error = function(e) {
+        warning(paste0("ERROR: [", resource, "] ", e))
+      }
+    )
+  }
+}
+
+
+#---- catalogue manipulation ----#
+
+# subset catalogue to only resources with data for a given year
+catalogue_for_year <- function(catalogue, year) {
+  keepers <- c()
+  for (resource in catalogue$f_resource) {
+    df_content <- get_years(resource)
+    if (year %in% df_content$f_year) {
+      keepers <- c(keepers, resource)
+    }
+  }
+  result <- catalogue |>
+    filter(f_resource %in% keepers) |>
+    mutate(data_year = year)
+  return(result)
+}
+
+# add a json column to catalogue that lists filters available for each resource
 catalogue_filters <- function(catalogue, year, level) {
-  # get filters for each data set
   catalogue$filters_json <- apply(catalogue, 1, function(x) {
-    query_filters(
+    get_filters(
       resource = x["f_resource"],
       year = year,
       level = level
@@ -67,6 +169,32 @@ catalogue_filters <- function(catalogue, year, level) {
   return(catalogue)
 }
 
+# create a dictionary of filters and values available for all resources
+filter_labels <- function(catalogue) {
+  result <- catalogue %>%
+    as_tibble() %>%
+    mutate(
+      filters = map(
+        as.character(filters_json),
+        ~ fromJSON(.x, simplifyVector = FALSE)
+      )
+    ) %>%
+    unnest_longer(filters) %>%
+    unnest_wider(filters, names_sep = "_") %>%
+    unnest_longer(filters_field_values) %>%
+    unnest_wider(filters_field_values, names_sep = "_") %>%
+    rename(
+      field = filters_field,
+      value = filters_field_values_value,
+      field_label = filters_field_label,
+      value_label = filters_field_values_label
+    ) %>%
+    select(-filters_json)
+
+  return(result)
+}
+
+# expand the catalogue with a row for all combinations of filter values for every resource
 expand_catalogue <- function(catalogue) {
   result <- catalogue %>%
     as_tibble() %>%
@@ -97,39 +225,17 @@ expand_catalogue <- function(catalogue) {
   return(result)
 }
 
-expand_catalogue_alt <- function(catalogue) {
-  result <- catalogue %>%
-    as_tibble() %>%
-    mutate(
-      filters = map(
-        as.character(filters_json),
-        ~ fromJSON(.x, simplifyVector = FALSE)
-      )
-    ) %>%
-    unnest_longer(filters) %>%
-    unnest_wider(filters, names_sep = "_") %>%
-    unnest_longer(filters_field_values) %>%
-    unnest_wider(filters_field_values, names_sep = "_") %>%
-    rename(
-      field = filters_field,
-      value = filters_field_values_value,
-      field_label = filters_field_label,
-      value_label = filters_field_values_label
-    ) %>%
-    select(-filters_json)
-
-  return(result)
-}
-
+# retrieve data for all rows of an expanded catalogue
 catalogue_data <- function(catalogue, year, level) {
   results <- list()
 
   filters <- names(catalogue) %>%
     {
-      .[(which(. == "f_short_description") + 1):length(.)]
+      .[(which(. == "data_year") + 1):length(.)]
     }
 
   pb <- txtProgressBar(min = 1, max = nrow(catalogue), style = 3)
+  start_time <- Sys.time()
   for (i in 1:nrow(catalogue)) {
     setTxtProgressBar(pb, i)
     resource <- catalogue$f_resource[i]
@@ -145,67 +251,20 @@ catalogue_data <- function(catalogue, year, level) {
       )
     }
 
-    X_JSON <- toJSON(list(
+    x_specs <- list(
       source = resource,
       conditions = conditions
-    ), auto_unbox = TRUE)
+    )
 
     results[[resource]] <- get_data(
       resource = resource,
       year = year,
       level = level,
-      X_JSON = X_JSON
+      x_specs = x_specs
     )
   }
   close(pb)
-  result <- bind_rows(results)
-  return(result)
-}
-
-
-get_data <- function(resource, year, level, X_JSON) {
-  response <- httr::GET(
-    url = "https://api.mapineq.org",
-    path = "functions/postgisftw.get_x_data/items.json",
-    query = list(
-      `_level` = level,
-      `_year` = year,
-      X_JSON = X_JSON,
-      limit = 10e3
-    ),
-    config = config(ssl_verifypeer = FALSE) # TODO: REMOVE WHEN SSL RENEWED
-  )
-
-  if (!status_code(response) == 200) {
-    message(paste0("[", resource, "] Error:", status_code(response)))
-  } else {
-    df_content <- jsonlite::fromJSON(content(response, "text", encoding = "UTF-8"))
-    if (is.data.frame(df_content)) {
-      df_content <- df_content |>
-        dplyr::mutate(indicator = resource) |>
-        dplyr::rename(value = x)
-
-      return(df_content)
-    } else {
-      message(paste0("No data returned for: \n", X_JSON))
-    }
-  }
-}
-
-query_filters <- function(resource, year, level) {
-  response <- httr::GET(
-    url = "https://api.mapineq.org",
-    path = "functions/postgisftw.get_column_values_source_json/items.json",
-    query = list(
-      `_resource` = resource,
-      source_selections = toJSON(list(
-        year = year,
-        level = level,
-        selected = list()
-      ), auto_unbox = TRUE)
-    ),
-    config = config(ssl_verifypeer = FALSE) # TODO: REMOVE WHEN SSL RENEWED
-  )
-  result <- content(response, "text", encoding = "UTF-8")
-  return(result)
+  end_time <- Sys.time()
+  message("Time elapsed: ", round(end_time - start_time, 2))
+  return(results)
 }
